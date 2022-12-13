c9296c87c81eb1602830079c3246713a7cbd6a2b
Joshua Borseth <joshuaborseth@gmail.com>
2022-12-13 14:58:56 -0800

    should work now with uploading through trpc?

--- src/components/Upload.tsx
+++ src/components/Upload.tsx
@@ -3,40 +3,30 @@ import { trpc } from "../utils/trpc";
 
 export default function Upload() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const { mutate } = trpc.sheet.createSheet.useMutation();
+  const { mutateAsync: createPresignedUrl } =
+    trpc.sheet.createSheet.useMutation();
   const uploadPhoto = async (file: File, e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const filename = encodeURIComponent(file.name);
-    const fileType = encodeURIComponent(file.type);
-    const res = await fetch(
-      `/api/s3/upload?file=${filename}&fileType=${fileType}`
-    );
-    const { url, fields } = await res.json();
+    if (!file) return;
+    const { url, fields }: { url: string; fields: any } =
+      (await createPresignedUrl({
+        fileName: file.name,
+        fileType: file.type,
+        fileUrl: `https://free-drumline-music-sheets.s3.us-west-2.amazonaws.com/${file.name}`,
+      })) as any;
+    const data = {
+      ...fields,
+      "Content-Type": file.type,
+      file,
+    };
     const formData = new FormData();
-
-    Object.entries({ ...fields, file }).forEach(([key, value]) => {
-      formData.append(key, value as string);
-    });
-    const upload = await fetch(url, {
+    for (const name in data) {
+      formData.append(name, data[name]);
+    }
+    await fetch(url, {
       method: "POST",
       body: formData,
     });
-    mutate(
-      {
-        fileUrl: `https://free-drumline-music-sheets.s3.us-west-2.amazonaws.com/${filename}`,
-      },
-      {
-        onSuccess: () => {
-          if (upload.ok) {
-            if (!inputRef.current) return;
-            inputRef.current.value = "";
-            console.log("Upload successful!");
-          } else {
-            console.error("Upload failed.");
-          }
-        },
-      }
-    );
   };
   return (
     <form
--- src/server/trpc/router/sheet.ts
+++ src/server/trpc/router/sheet.ts
@@ -1,11 +1,36 @@
 import { z } from "zod";
 import { router, publicProcedure } from "../trpc";
+import { AWS } from "../../../libs/aws";
+const s3 = new AWS.S3();
 
 export const sheetRouter = router({
   createSheet: publicProcedure
-    .input(z.object({ fileUrl: z.string() }))
+    .input(
+      z.object({
+        fileUrl: z.string(),
+        fileName: z.string(),
+        fileType: z.string(),
+      })
+    )
     .mutation(({ ctx, input }) => {
-      const sheet = ctx.prisma.sheet.create({ data: input });
-      return sheet;
+      return new Promise((resolve, reject) => {
+        s3.createPresignedPost(
+          {
+            Fields: {
+              key: input.fileName,
+              "Content-Type": input.fileType,
+            },
+            Conditions: [
+              ["content-length-range", 0, 1048576], // up to 1 MB
+            ],
+            Expires: 60,
+            Bucket: process.env.BUCKET_NAME,
+          },
+          (err, signed) => {
+            if (err) return reject(err);
+            resolve(signed);
+          }
+        );
+      });
     }),
 });
