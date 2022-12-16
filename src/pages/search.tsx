366a2d8dc97aa03e438ac4ee36ae3b6b23f62578
Joshua Borseth <joshuaborseth@gmail.com>
2022-12-15 16:07:03 -0800

    refactored search component to be styled different and use a ref instead of usestate

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -3,13 +3,13 @@ import { type NextPage } from "next";
 import DesktopNav from "../components/Drawer/DesktopNav";
 import Footer from "../components/Footer";
 import MobileNav from "../components/Drawer/MobileNav";
-import { useState } from "react";
+import { type FormEvent, useRef } from "react";
 import Results from "../components/Results";
 import Head from "next/head";
 const Search: NextPage = () => {
-  const [search, setSearch] = useState("");
+  const searchRef = useRef<HTMLInputElement>(null);
   return (
-    <div>
+    <>
       <Head>
         <title>FDL | Search</title>
       </Head>
@@ -17,37 +17,38 @@ const Search: NextPage = () => {
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           <DesktopNav />
-          <main className="relative grid min-h-screen">
+          <main className="relative mx-auto grid min-h-screen w-2/3">
             <form
-              onSubmit={(e: any) => {
+              onSubmit={(e: FormEvent<HTMLFormElement>) => {
                 e.preventDefault();
-                setSearch(e.target[0].value);
+                if (searchRef.current) {
+                  console.log("make request to get some sheets back");
+                }
               }}
-              className="relative mt-10 flex h-screen w-5/6 flex-wrap justify-center justify-self-center lg:w-2/3"
+              className="flex items-center justify-center gap-2"
             >
               <input
                 type="text"
-                placeholder="🔍 Search"
-                className="z-20 h-10 w-full rounded-md border px-2 text-2xl shadow-lg focus:outline-none"
+                placeholder="Search..."
+                className="w-full rounded-md border px-3 py-2 text-2xl shadow-lg"
+                ref={searchRef}
               />
               <button
-                className="absolute top-0 right-0 z-20 mt-2 mr-1 rounded bg-base-200 px-1 text-xs text-base-content"
-                onDoubleClick={(e: any) => {
-                  e.preventDefault();
-                  e.target.form[0].value = "";
-                  setSearch("");
+                className="rounded-md border bg-accent px-6 py-2 text-lg text-base-100 shadow-lg"
+                type="button"
+                onClick={() => {
+                  if (searchRef.current) searchRef.current.value = "";
                 }}
               >
                 Clear
               </button>
-              <Results input={search} />
             </form>
           </main>
           <Footer />
         </div>
         <MobileNav />
       </div>
-    </div>
+    </>
   );
 };
 export default Search;
